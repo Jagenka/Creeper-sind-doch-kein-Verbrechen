602e08a885f8fd85f6ca2535e6636c86afc3da69
Jagenka <dev@jayhannes.de>
2025-01-20 17:08:37 +0100

    add fireworks to creeper explosions

--- src/main/java/de/jagenka/mixin/ServerWorldMixin.java
+++ src/main/java/de/jagenka/mixin/ServerWorldMixin.java
@@ -1,11 +1,23 @@
 package de.jagenka.mixin;
 
+import de.jagenka.CreeperSindDochKeinVerbrechen;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.ExplosionBehavior;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin
@@ -27,4 +39,15 @@ public abstract class ServerWorldMixin
             return getGameRules().getBoolean(rule);
         }
     }
+
+    @Inject(method = "createExplosion", at = @At("TAIL"))
+    private void showAlternateCreeperExplosionEffect(@Nullable Entity entity, @Nullable DamageSource damageSource, @Nullable ExplosionBehavior behavior, double x, double y, double z, float power, boolean createFire, World.ExplosionSourceType explosionSourceType, ParticleEffect smallParticle, ParticleEffect largeParticle, RegistryEntry<SoundEvent> soundEvent, CallbackInfo ci)
+    {
+        if (entity instanceof CreeperEntity)
+        {
+            CreeperSindDochKeinVerbrechen.INSTANCE.createFireworkExplosionEffect((ServerWorld) (Object) this, x, y, z);
+        }
+    }
+
+
 }
--- src/main/kotlin/de/jagenka/CreeperSindDochKeinVerbrechen.kt
+++ src/main/kotlin/de/jagenka/CreeperSindDochKeinVerbrechen.kt
@@ -1,15 +1,57 @@
 package de.jagenka
 
+import it.unimi.dsi.fastutil.ints.IntList
 import net.fabricmc.api.ModInitializer
+import net.minecraft.component.DataComponentTypes
+import net.minecraft.component.type.FireworkExplosionComponent
+import net.minecraft.component.type.FireworksComponent
+import net.minecraft.entity.projectile.FireworkRocketEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.DyeColor
 import org.slf4j.LoggerFactory
 
-object CreeperSindDochKeinVerbrechen : ModInitializer {
+object CreeperSindDochKeinVerbrechen : ModInitializer
+{
     private val logger = LoggerFactory.getLogger("creeper-sind-doch-kein-verbrechen")
 
-	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Creeper sind doch kein Verbrechen Mod loaded!")
-	}
+    override fun onInitialize()
+    {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        logger.info("Creeper sind doch kein Verbrechen Mod loaded!")
+    }
+
+    fun createFireworkExplosionEffect(serverWorld: ServerWorld, x: Double, y: Double, z: Double)
+    {
+        val fireworkItemStack = ItemStack(Items.FIREWORK_ROCKET)
+        fireworkItemStack.set<FireworksComponent>(
+            DataComponentTypes.FIREWORKS,
+            FireworksComponent(
+                0,
+                listOf(
+                    FireworkExplosionComponent(
+                        FireworkExplosionComponent.Type.CREEPER,
+                        IntList.of(DyeColor.LIME.fireworkColor),
+                        IntList.of(), // no trail
+                        false,
+                        false
+                    ),
+                    FireworkExplosionComponent(
+                        FireworkExplosionComponent.Type.CREEPER,
+                        IntList.of(DyeColor.GREEN.fireworkColor),
+                        IntList.of(), // no trail
+                        false,
+                        false
+                    )
+                )
+            )
+        )
+
+        val fireworkRocket = FireworkRocketEntity(serverWorld, x, y + 3, z, fireworkItemStack)
+        serverWorld.spawnEntity(fireworkRocket)
+        fireworkRocket.explodeAndRemove(serverWorld)
+    }
 }
