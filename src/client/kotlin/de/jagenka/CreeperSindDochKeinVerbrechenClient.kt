16a4a8519eafeaa1aac2179f9ccc63437ab0de35
Jagenka <dev@jayhannes.de>
2025-01-20 14:49:56 +0100

    change basic structure

--- src/client/kotlin/de/jagenka/CreeperSindDochKeinVerbrechenClient.kt
+++
@@ -1,9 +0,0 @@
-package de.jagenka
-
-import net.fabricmc.api.ClientModInitializer
-
-object CreeperSindDochKeinVerbrechenClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
